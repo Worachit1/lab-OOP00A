07e1d01d84e41c4ff877d53514dab0979db21c93
Worachit1 <worachit.t@kkumail.com>
2022-11-28 15:34:44 +0700

    add nameid

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // print string to commanline
-Console.WriteLine("Woeachit Thonglert");
+Console.WriteLine("Worachit Thonglert");
+//id
+Console.WriteLine("653450298-2");
+//email
+Console.WriteLine("worachit.t@kkumail.com");
